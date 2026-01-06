143e1573d61c7baa7db744735191df13a71c9844
mitsu1119 <hachimitsu1119@gmail.com>
2026-01-06 18:16:14 +0900

    とりあえず articleIndex を返すようにした

--- apps/bff/src/index.ts
+++ apps/bff/src/index.ts
@@ -1,31 +1,16 @@
+import { articleIndex } from "./generated/articles";
+
 export default {
 	async fetch(_req: Request, _env: Env, _ctx: ExecutionContext): Promise<Response> {
 		const url = new URL(_req.url);
 
-		// GET /api/articles -> hello
+		// GET /api/articles -> articlesIndex
 		if(url.pathname === "/api/articles" && _req.method === "GET") {
-			return new Response(JSON.stringify({ hello: "hello" }), {
-				status: 200,
-				headers: {
-					"content-type": "application/JSON; charset=utf-8",
-					"access-control-allow-origin": "*"
-				}
+			return new Response(JSON.stringify(articleIndex, null, 2), {
+				headers: { "content-type": "application/json; charset=utf-8" }
 			});
 		}
 
-		// OPTIONS (CORS preflight) も一応返す
-   		 if (url.pathname.startsWith("/api/") && _req.method === "OPTIONS") {
-     		return new Response(null, {
-        		status: 204,
-        		headers: {
-          			"access-control-allow-origin": "*",
-          			"access-control-allow-methods": "GET,OPTIONS",
-         			"access-control-allow-headers": "content-type",
-          			"access-control-max-age": "86400"
-        		}
-      		});
-    	}
-		
 	    return new Response("Not Found", { status: 404 });
 	}
-}
+};
